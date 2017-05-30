0d7696d4a23bfa39d23b19aa4b2a4bb76675fd02
Kei Nohguchi <kei@nohguchi.com>
2017-05-30 14:12:38 -0700

    main.tf: Create a single droplet with private IP and IPv6 enabled

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "do_token" {}
+variable "do_ssh_key" {}
+
+# https://www.terraform.io/docs/providers/do/index.html
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
+
+# https://www.terraform.io/docs/providers/do/r/droplet.html
+resource "digitalocean_droplet" "web" {
+  image = "ubuntu-16-04-x64"
+  name = "web-1"
+  region = "nyc3"
+  size = "512mb"
+  ipv6 = true
+  private_networking = true
+  ssh_keys = ["${var.do_ssh_key}"]
+}
