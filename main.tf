e6e813eac42d09c33cdc052161e016f34aa747aa
Kei Nohguchi <kei@nohguchi.com>
2017-06-01 05:08:00 -0700

    main.tf: Run HTTP on port 8080 through DO's user_data!

--- main.tf
+++ main.tf
@@ -20,21 +20,12 @@ resource "digitalocean_droplet" "server" {
   private_networking = true
   ssh_keys           = ["${var.DO_FINGERPRINT}"]
   tags               = ["${digitalocean_tag.server.id}"]
+  user_data          = <<EOF
+#!/bin/bash
 
-  connection {
-    user        = "root"
-    type        = "ssh"
-    private_key = "${var.DO_PRIVATE_KEY}"
-    timeout     = "2m"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "apt-get update",
-      "apt-get -y install python",
-      "apt-get -y install iperf3"
-    ]
-  }
+echo "Hello World!" > index.html
+nohup busybox httpd -f -p 8080 0<&- &> /tmp/script.log &
+EOF
 }
 
 # https://www.terraform.io/docs/providers/do/r/tag.html
