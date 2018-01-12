c776779f09cc9e19654187723b6e5673a598bab3
Kei Nohguchi <kei@nohguchi.com>
2018-01-11 22:04:06 -0800

    firewalls: Support multiple client/server handling
    As of this commit, there is only one firewalls rule
    which applies to all the client/server sets.

--- firewalls/both/000-deny-all/outputs.tf
+++ firewalls/both/000-deny-all/outputs.tf
@@ -1,7 +1,3 @@
-output "server_flip" {
-  value = "${data.terraform_remote_state.flips.server_flip}"
-}
-
 output "server_port" {
   value = "${data.terraform_remote_state.root.server_port}"
 }
@@ -14,42 +10,78 @@ output "server_tag_id" {
   value = "${data.terraform_remote_state.root.server_tag_id}"
 }
 
-output "client0_public_ipv4" {
-  value = "${data.terraform_remote_state.root.client0_public_ipv4}"
+output "client_count" {
+  value = "${data.terraform_remote_state.root.client_count}"
 }
 
-output "client0_private_ipv4" {
-  value = "${data.terraform_remote_state.root.client0_private_ipv4}"
+output "server_count" {
+  value = "${data.terraform_remote_state.root.server_count}"
 }
 
-output "client0_public_ipv6" {
-  value = "${data.terraform_remote_state.root.client0_public_ipv6}"
+output "monitor_count" {
+  value = "${data.terraform_remote_state.root.monitor_count}"
 }
 
-output "server0_public_ipv4" {
-  value = "${data.terraform_remote_state.root.server0_public_ipv4}"
+output "client_droplet_id" {
+  value = "${data.terraform_remote_state.root.client_droplet_id}"
 }
 
-output "server0_private_ipv4" {
-  value = "${data.terraform_remote_state.root.server0_private_ipv4}"
+output "client_droplet_region" {
+  value = "${data.terraform_remote_state.root.client_droplet_region}"
 }
 
-output "server0_public_ipv6" {
-  value = "${data.terraform_remote_state.root.server0_public_ipv6}"
+output "client_public_ipv4" {
+  value = "${data.terraform_remote_state.root.client_public_ipv4}"
 }
 
-output "monitor0_droplet_region" {
-  value = "${data.terraform_remote_state.root.monitor0_droplet_region}"
+output "client_private_ipv4" {
+  value = "${data.terraform_remote_state.root.client_private_ipv4}"
 }
 
-output "monitor0_public_ipv4" {
-  value = "${data.terraform_remote_state.root.monitor0_public_ipv4}"
+output "client_public_ipv6" {
+  value = "${data.terraform_remote_state.root.client_public_ipv6}"
 }
 
-output "monitor0_private_ipv4" {
-  value = "${data.terraform_remote_state.root.monitor0_private_ipv4}"
+output "server_droplet_id" {
+  value = "${data.terraform_remote_state.root.server_droplet_id}"
 }
 
-output "monitor0_public_ipv6" {
-  value = "${data.terraform_remote_state.root.monitor0_public_ipv6}"
+output "server_droplet_region" {
+  value = "${data.terraform_remote_state.root.server_droplet_region}"
+}
+
+output "server_public_ipv4" {
+  value = "${data.terraform_remote_state.root.server_public_ipv4}"
+}
+
+output "server_private_ipv4" {
+  value = "${data.terraform_remote_state.root.server_private_ipv4}"
+}
+
+output "server_public_ipv6" {
+  value = "${data.terraform_remote_state.root.server_public_ipv6}"
+}
+
+output "server_flip" {
+  value = "${data.terraform_remote_state.flips.server_flip}"
+}
+
+output "monitor_droplet_id" {
+  value = "${data.terraform_remote_state.root.monitor_droplet_id}"
+}
+
+output "monitor_droplet_region" {
+  value = "${data.terraform_remote_state.root.monitor_droplet_region}"
+}
+
+output "monitor_public_ipv4" {
+  value = "${data.terraform_remote_state.root.monitor_public_ipv4}"
+}
+
+output "monitor_private_ipv4" {
+  value = "${data.terraform_remote_state.root.monitor_private_ipv4}"
+}
+
+output "monitor_public_ipv6" {
+  value = "${data.terraform_remote_state.root.monitor_public_ipv6}"
 }
